a20f5823cb35d87f183663690066214c31cc4550
mingshuisheng <1820924213@qq.com>
2023-10-26 21:09:06 +0800

    5.2: DrawRectangle四种Line Join的效果

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use windows::core::w;
+use windows::core::{IntoParam, w};
 use windows::Win32::Graphics::Direct2D::Common::{D2D1_COLOR_F, D2D_RECT_F};
+use windows::Win32::Graphics::Direct2D::{D2D1_CAP_STYLE_ROUND, D2D1_CAP_STYLE_SQUARE, D2D1_CAP_STYLE_TRIANGLE, D2D1_DASH_STYLE_DASH, D2D1_DASH_STYLE_SOLID, D2D1_LINE_JOIN, D2D1_LINE_JOIN_BEVEL, D2D1_LINE_JOIN_MITER, D2D1_LINE_JOIN_MITER_OR_BEVEL, D2D1_LINE_JOIN_ROUND, D2D1_STROKE_STYLE_PROPERTIES, ID2D1Brush, ID2D1DeviceContext, ID2D1Factory1, ID2D1StrokeStyle};
 
 mod d2d;
 mod graphic;
@@ -7,6 +8,11 @@ mod graphic;
 fn main() {
     d2d::init_com();
     let graphic = graphic::Graphic::new();
+
+    let style1 = create_stroke_style(&graphic.d2d_factory, D2D1_LINE_JOIN_MITER, 0.0);
+    let style2 = create_stroke_style(&graphic.d2d_factory, D2D1_LINE_JOIN_ROUND, 0.0);
+    let style3 = create_stroke_style(&graphic.d2d_factory, D2D1_LINE_JOIN_BEVEL, 0.0);
+    let style4 = create_stroke_style(&graphic.d2d_factory, D2D1_LINE_JOIN_MITER_OR_BEVEL, 0.0);
 
     graphic.draw_and_save((640, 480), w!("output.png"), |ctx| unsafe {
         ctx.Clear(Some(&D2D1_COLOR_F {
@@ -24,12 +30,44 @@ fn main() {
         }, None).unwrap();
 
         let rect = D2D_RECT_F {
-            left: 100.0,
-            top: 100.0,
-            right: 300.0,
-            bottom: 300.0,
+            left: 50.0,
+            top: 50.0,
+            right: 150.0,
+            bottom: 150.0,
         };
 
-        ctx.FillRectangle(&rect, &brush)
+        let stroke_width = 20.0;
+        let x_offset = 150.0;
+        let y_offset = 0.0;
+        draw_rectangle(ctx, &rect, &brush, stroke_width, &style1, x_offset * 0.0, y_offset);
+        draw_rectangle(ctx, &rect, &brush, stroke_width, &style2, x_offset * 1.0, y_offset);
+        draw_rectangle(ctx, &rect, &brush, stroke_width, &style3, x_offset * 2.0, y_offset);
+        draw_rectangle(ctx, &rect, &brush, stroke_width, &style4, x_offset * 3.0, y_offset);
     });
+}
+
+fn draw_rectangle(ctx: &ID2D1DeviceContext, rect: &D2D_RECT_F, brush: impl IntoParam<ID2D1Brush>, stroke_width: f32, style: &ID2D1StrokeStyle, x_offset: f32, y_offset: f32) {
+    let mut rect = rect.clone();
+    rect.left += x_offset;
+    rect.right += x_offset;
+    rect.top += y_offset;
+    rect.bottom += y_offset;
+    unsafe {
+        ctx.DrawRectangle(&rect, brush, stroke_width, style);
+    }
+}
+
+fn create_stroke_style(d2d_factory: &ID2D1Factory1, line_join: D2D1_LINE_JOIN, miter_limit: f32) -> ID2D1StrokeStyle {
+    unsafe {
+        d2d_factory.CreateStrokeStyle(&D2D1_STROKE_STYLE_PROPERTIES {
+            // startCap: D2D1_CAP_STYLE_ROUND,
+            // endCap: D2D1_CAP_STYLE_TRIANGLE,
+            // dashCap: D2D1_CAP_STYLE_TRIANGLE,
+            lineJoin: line_join,
+            miterLimit: miter_limit,
+            dashStyle: D2D1_DASH_STYLE_SOLID,
+            // dashOffset: 1.0,
+            ..Default::default()
+        }, None).unwrap()
+    }
 }
