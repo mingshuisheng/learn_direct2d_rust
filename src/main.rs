4e641ba81146851911524a6c0fe9bd574c4c9dd1
mingshuisheng <1820924213@qq.com>
2023-10-28 18:58:03 +0800

    6.2: 演示填充椭圆的效果

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use windows::core::w;
-use windows::Win32::Graphics::Direct2D::Common::D2D1_COLOR_F;
+use windows::Win32::Graphics::Direct2D::Common::{D2D1_COLOR_F, D2D_POINT_2F};
+use windows::Win32::Graphics::Direct2D::D2D1_ELLIPSE;
 
 mod d2d;
 mod graphic;
@@ -16,8 +17,26 @@ fn main() {
             a: 1.0,
         }));
 
+
+        let brush = ctx.CreateSolidColorBrush(&D2D1_COLOR_F{
+            r: 1.0,
+            g: 0.0,
+            b: 0.0,
+            a: 1.0,
+        }, None).unwrap();
+
         //RoundedRectangle
         //Ellipse
 
+        let e = D2D1_ELLIPSE{
+            point: D2D_POINT_2F{
+                x: 100.0,
+                y: 100.0
+            },
+            radiusX: 20.0,
+            radiusY: 30.0,
+        };
+
+        ctx.FillEllipse(&e, &brush);
     });
 }
