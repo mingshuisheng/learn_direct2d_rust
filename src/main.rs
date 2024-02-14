4233afdeb9c6a882d084411acdd8cf024ab6f1de
mingshuisheng <1820924213@qq.com>
2024-02-14 17:02:59 +0800

    DrawTextLayout文本的装饰

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::iter::once;
 use windows::core::w;
 use windows::Win32::Graphics::Direct2D::Common::{D2D1_COLOR_F, D2D_POINT_2F};
 use windows::Win32::Graphics::Direct2D::D2D1_DRAW_TEXT_OPTIONS_CLIP;
-use windows::Win32::Graphics::DirectWrite::{DWRITE_FACTORY_TYPE_SHARED, DWRITE_FONT_STRETCH_NORMAL, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_WEIGHT_ULTRA_BLACK, DWRITE_WORD_WRAPPING_NO_WRAP, DWriteCreateFactory, IDWriteFactory2};
+use windows::Win32::Graphics::DirectWrite::{DWRITE_FACTORY_TYPE_SHARED, DWRITE_FONT_FEATURE, DWRITE_FONT_FEATURE_TAG_ALTERNATE_ANNOTATION_FORMS, DWRITE_FONT_FEATURE_TAG_ALTERNATIVE_FRACTIONS, DWRITE_FONT_FEATURE_TAG_CAPITAL_SPACING, DWRITE_FONT_FEATURE_TAG_CASE_SENSITIVE_FORMS, DWRITE_FONT_FEATURE_TAG_CONTEXTUAL_ALTERNATES, DWRITE_FONT_FEATURE_TAG_CONTEXTUAL_LIGATURES, DWRITE_FONT_FEATURE_TAG_GLYPH_COMPOSITION_DECOMPOSITION, DWRITE_FONT_FEATURE_TAG_LINING_FIGURES, DWRITE_FONT_FEATURE_TAG_LOCALIZED_FORMS, DWRITE_FONT_FEATURE_TAG_MARK_POSITIONING, DWRITE_FONT_FEATURE_TAG_MARK_TO_MARK_POSITIONING, DWRITE_FONT_FEATURE_TAG_MATHEMATICAL_GREEK, DWRITE_FONT_FEATURE_TAG_NLC_KANJI_FORMS, DWRITE_FONT_FEATURE_TAG_OLD_STYLE_FIGURES, DWRITE_FONT_FEATURE_TAG_ORDINALS, DWRITE_FONT_FEATURE_TAG_PETITE_CAPITALS, DWRITE_FONT_FEATURE_TAG_PETITE_CAPITALS_FROM_CAPITALS, DWRITE_FONT_FEATURE_TAG_PROPORTIONAL_ALTERNATE_WIDTH, DWRITE_FONT_FEATURE_TAG_PROPORTIONAL_FIGURES, DWRITE_FONT_FEATURE_TAG_PROPORTIONAL_WIDTHS, DWRITE_FONT_FEATURE_TAG_QUARTER_WIDTHS, DWRITE_FONT_FEATURE_TAG_REQUIRED_LIGATURES, DWRITE_FONT_FEATURE_TAG_RUBY_NOTATION_FORMS, DWRITE_FONT_FEATURE_TAG_SCIENTIFIC_INFERIORS, DWRITE_FONT_FEATURE_TAG_SMALL_CAPITALS, DWRITE_FONT_FEATURE_TAG_STANDARD_LIGATURES, DWRITE_FONT_FEATURE_TAG_STYLISTIC_ALTERNATES, DWRITE_FONT_FEATURE_TAG_STYLISTIC_SET_1, DWRITE_FONT_FEATURE_TAG_STYLISTIC_SET_7, DWRITE_FONT_STRETCH_NORMAL, DWRITE_FONT_STYLE_NORMAL, DWRITE_FONT_WEIGHT_NORMAL, DWRITE_FONT_WEIGHT_ULTRA_BLACK, DWRITE_TEXT_RANGE, DWRITE_WORD_WRAPPING_NO_WRAP, DWriteCreateFactory, IDWriteFactory2};
 
 mod d2d;
 mod graphic;
@@ -29,13 +29,14 @@ fn main() {
         a: 1.0,
     };
 
-    let text = "Hello, world!";
+    let text = "Hello, World!";
+    let text_length = text.chars().count() as u32;
     let text_vec = text.encode_utf16().chain(once(0)).collect::<Vec<u16>>();
     let text_format = unsafe {
         write_factory.CreateTextFormat(
             font_family,
             None,
-            DWRITE_FONT_WEIGHT_ULTRA_BLACK,
+            DWRITE_FONT_WEIGHT_NORMAL,
             DWRITE_FONT_STYLE_NORMAL,
             DWRITE_FONT_STRETCH_NORMAL,
             font_size,
@@ -43,7 +44,7 @@ fn main() {
         ).unwrap()
     };
 
-    let text_layout = unsafe {
+    let create_text_layout = || unsafe {
         write_factory.CreateTextLayout(&text_vec, &text_format, 640.0, 480.0).unwrap()
     };
 
@@ -52,10 +53,45 @@ fn main() {
         y: 0.0,
     };
 
-    graphic.draw_and_save((640, 480), w!("draw_text_layout.png"), |ctx| unsafe {
+    graphic.draw_and_save((640, 480), w!("underline.png"), |ctx| unsafe {
         ctx.Clear(Some(&graphic_color));
         //text
         let brush = ctx.CreateSolidColorBrush(&text_color, None).unwrap();
+
+        let text_layout = create_text_layout();
+        text_layout.SetUnderline(true, DWRITE_TEXT_RANGE { startPosition: 0, length: text_length }).unwrap();
+        ctx.DrawTextLayout(origin, &text_layout, &brush, D2D1_DRAW_TEXT_OPTIONS_CLIP);
+    });
+
+    graphic.draw_and_save((640, 480), w!("strikethrough.png"), |ctx| unsafe {
+        ctx.Clear(Some(&graphic_color));
+        //text
+        let brush = ctx.CreateSolidColorBrush(&text_color, None).unwrap();
+        let text_layout = create_text_layout();
+        text_layout.SetStrikethrough(true, DWRITE_TEXT_RANGE { startPosition: 0, length: text_length }).unwrap();
+        ctx.DrawTextLayout(origin, &text_layout, &brush, D2D1_DRAW_TEXT_OPTIONS_CLIP);
+    });
+
+    graphic.draw_and_save((640, 480), w!("typography.png"), |ctx| unsafe {
+        ctx.Clear(Some(&graphic_color));
+        //text
+        let brush = ctx.CreateSolidColorBrush(&text_color, None).unwrap();
+        let text_layout = create_text_layout();
+        ctx.DrawTextLayout(origin, &text_layout, &brush, D2D1_DRAW_TEXT_OPTIONS_CLIP);
+
+        let typography = write_factory.CreateTypography().unwrap();
+
+        typography.AddFontFeature(DWRITE_FONT_FEATURE {
+            //这个地方我是一个个值试了，没有看到效果
+            nameTag: DWRITE_FONT_FEATURE_TAG_STANDARD_LIGATURES,
+            parameter: 1,
+        }).unwrap();
+
+        text_layout.SetTypography(&typography, DWRITE_TEXT_RANGE { startPosition: 0, length: text_length }).unwrap();
+        let origin = D2D_POINT_2F {
+            x: 0.0,
+            y: 100.0,
+        };
         ctx.DrawTextLayout(origin, &text_layout, &brush, D2D1_DRAW_TEXT_OPTIONS_CLIP);
     });
 }
